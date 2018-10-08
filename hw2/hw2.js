ba3e7ca6b1d13d0cac1b36bc39005c6d4640dbdd
Yumin Chen <bk201@YumindeMac-Pro.local>
2018-10-08 03:40:28 -0700

    Address Mask Calculation Added

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,4 +1,9 @@
 
+/**
+ * Verify IP Type(Public/Private)
+ * @param {*} ipF 1st 8bits
+ * @param {*} ipS 2nd 8bits
+ */
 function ipType(ipF, ipS){
     'use strict'
     if(ipF == 192 && ipS == 168) {
@@ -16,14 +21,24 @@ function ipType(ipF, ipS){
     }
 }
 
-function maskToBits
+/**
+ * Return the number of available address.
+ * @param {*} mask Network Mask
+ */
+function addCalc(mask){
+    if(mask==0){
+       alert('Please enter numbers!');
+    }else {
+        return Math.pow(2,32-mask);
+    } 
+}
 
 $('div').submit(function (event) {
     event.preventDefault();
     var ipp1 = $('#ip1').val().trim();
     var ipp2 = $('#ip2').val().trim();
     var result = ipType(ipp1,ipp2);
-
+    var number = addCalc($('#mask').val().trim());
     var p = document.getElementById('resultReturn');
-    p.innerHTML = result;
+    p.innerHTML = result+" "+number;
 });
