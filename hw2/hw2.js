49269330c6154e0b225d484b469636a080324b33
Yumin Chen <bk201@YumindeMac-Pro.local>
2018-10-08 03:15:36 -0700

    ipType JS function added and jQuery relationship added

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -1,6 +1,29 @@
-//'use strict';
 
+function ipType(ipF, ipS){
+    'use strict'
+    if(ipF == 192 && ipS == 168) {
+        return 'Class C Private IP';
+    }else if(ipF == 172 && ipS == 16){
+        return 'Class B Private IP';
+    }else if(ipF == 10){
+        return 'Class A Private IP';
+    }else if(ipF == 127){
+        return 'Loop Address';     
+    }else if(ipF == null){
+        return 'Null';
+    }else {
+        return ipF;
+    }
+}
 
+function maskToBits
 
-var p = document.getElementById('p1');
-p.innerHTML = 'Test AP';
+$('div').submit(function (event) {
+    event.preventDefault();
+    var ipp1 = $('#ip1').val().trim();
+    var ipp2 = $('#ip2').val().trim();
+    var result = ipType(ipp1,ipp2);
+
+    var p = document.getElementById('resultReturn');
+    p.innerHTML = result;
+});
