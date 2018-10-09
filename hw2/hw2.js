a54c4fa5af5a696625a30908d280cabb46f02d0e
Yumin Chen <bk201@YumindeMac-Pro.local>
2018-10-08 21:45:36 -0700

    Range Cal Feature Add

--- hw2/hw2.js
+++ hw2/hw2.js
@@ -26,19 +26,59 @@ function ipType(ipF, ipS){
  * @param {*} mask Network Mask
  */
 function addCalc(mask){
-    if(mask==0){
-       alert('Please enter numbers!');
-    }else {
         return Math.pow(2,32-mask);
-    } 
 }
+
+var minIP = undefined;
+var maxIP = undefined;
+var network = undefined;
+/**
+ * Ip Range based on subnet mask.
+ * @param {*} ip1 1st 8bits
+ * @param {*} ip2 2nd 8bits 
+ * @param {*} ip3 3rd 8bits
+ * @param {*} ip4 4th 8bits
+ * @param {*} mask Net Mask Bits
+ */
+function ipRange(ip1,ip2,ip3,ip4,mask){
+    var maxCount = 0;
+    var avaAddr = addCalc(mask);
+    
+     if(mask>=24){
+        var i;
+        for(i = 0; i < 256; i = i + avaAddr)
+        {
+            if(ip4 < i){
+                minIP = ip1+'.'+ip2+'.'+ip3+'.'+(avaAddr-1);
+                maxIP = ip1+'.'+ip2+'.'+ip3+'.'+i; 
+            }else if(ip4 == i){
+                minIP = ip1+'.'+ip2+'.'+ip3+'.'+i;
+                maxIP = ip1+'.'+ip2+'.'+ip3+'.'+(i+avaAddr-1);
+            }
+        }
+     }else{
+        var ipArray = [ip1,ip2,ip3,ip4]; 
+        if(mask<=8){
+            network = ip1+'.'+
+        }
+
+     }
+    
+}
+
+
 
 $('div').submit(function (event) {
     event.preventDefault();
+    //get the ip address  and mask from text
     var ipp1 = $('#ip1').val().trim();
     var ipp2 = $('#ip2').val().trim();
+    var ipp3 = $('#ip3').val().trim();
+    var ipp4 = $('#ip4').val().trim();
+    var mask = $('#mask').val().trim();
+
     var result = ipType(ipp1,ipp2);
-    var number = addCalc($('#mask').val().trim());
-    var p = document.getElementById('resultReturn');
-    p.innerHTML = result+" "+number;
+    var number = addCalc(mask);
+    //var p = document.getElementById('resultReturn');
+    $('#resultReturn').html(result+" "+number);
 });
