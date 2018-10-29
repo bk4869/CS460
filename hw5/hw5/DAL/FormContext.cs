d1eed12d0267dcf2fe33217146f4a2396171a78c
YUMIN <ychen13@wou.edu>
2018-10-28 23:28:56 -0700

    HW5 Structure Setup

---
+++ hw5/hw5/DAL/FormContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using hw5.Models;
+
+namespace hw5.DAL
+{
+    public class FormContext : DbContext
+    {
+        public FormContext() : base("name=ApartDB")
+        {
+
+        }
+
+        public virtual DbSet<ApartRequestForm> ApartRequestForm { get; set; }
+
+    }
+}
---
+++ hw5/hw5/Models/ApartRequestForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace hw5.Models
+{
+    public class ApartRequestForm
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required, StringLength(20)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public string ApartName { get; set; }
+
+        [Required]
+        public int UninNum { get; set; }
+
+        public string CurrTime
+        {
+            get
+            {
+               return DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+            }
+        }
+    }
+}
