e32d956008db2c71506ad12e7c27db515f18cc3c
YUMIN <ychen13@wou.edu>
2018-11-09 02:28:04 -0800

    Search Done.

---
+++ hw6/hw6/Controllers/PeopleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hw6.Models;
+using hw6.Models.FrontViewModel;
+using System.Data;
+
+
+
+
+namespace hw6.Controllers
+{
+    public class PeopleController : Controller
+    {
+        WideWorldImportersContext db = new WideWorldImportersContext();
+
+        // GET: People
+        public ActionResult Index(String searchData)
+        {
+            FEViewModel vm = new FEViewModel();
+
+            searchData = Request.QueryString["searchBar"];
+
+            //searchData = "Cristina Longo";
+            Console.WriteLine("Reach GOT Data");
+
+            if (String.IsNullOrEmpty(searchData))
+            {
+                Console.WriteLine("Null OR EMPTY");
+                ViewBag.display = false;
+                return View();
+            }
+            else
+            {
+                ViewBag.display = true;
+                ViewBag.isValid = db.People.Count(p => p.FullName.Contains(searchData));
+                return View(db.People.Where(p => p.FullName.Contains(searchData)).ToList());
+            }
+        }
+
+        public ActionResult Info(int? id)
+        {
+            return View();
+        }
+    }
+}
---
+++ hw6/hw6/Models/FrontViewModel/FEViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace hw6.Models.FrontViewModel
+{
+    public class FEViewModel
+    {
+
+        public People People { get; set; }
+
+        public Customers Customers { get; set; }
+
+        public List<InvoiceLines> InvoiceLines { get; set; }
+    }
+}
