04b9d6dfc4faacc58d0a2110d4dd79382ee6be63
YUMIN <ychen13@wou.edu>
2018-11-10 02:01:42 -0800

    Details feature added. Final Ver1

--- hw6/hw6/Controllers/PeopleController.cs
+++ hw6/hw6/Controllers/PeopleController.cs
@@ -6,29 +6,31 @@ using System.Web.Mvc;
 using hw6.Models;
 using hw6.Models.FrontViewModel;
 using System.Data;
-
-
+using System.Data.Entity;
+using System.Net;
+using System.Diagnostics;
 
 
 namespace hw6.Controllers
 {
     public class PeopleController : Controller
     {
+
+        //Where to access DB
         WideWorldImportersContext db = new WideWorldImportersContext();
 
+        /// <summary>
+        /// Index Controller Method
+        /// </summary>
+        /// <param name="searchData"></param>
+        /// <returns></returns>
         // GET: People
         public ActionResult Index(String searchData)
         {
-            FEViewModel vm = new FEViewModel();
-
             searchData = Request.QueryString["searchBar"];
-
-            //searchData = "Cristina Longo";
-            Console.WriteLine("Reach GOT Data");
 
             if (String.IsNullOrEmpty(searchData))
             {
-                Console.WriteLine("Null OR EMPTY");
                 ViewBag.display = false;
                 return View();
             }
@@ -36,13 +38,65 @@ namespace hw6.Controllers
             {
                 ViewBag.display = true;
                 ViewBag.isValid = db.People.Count(p => p.FullName.Contains(searchData));
+                //Return related query
                 return View(db.People.Where(p => p.FullName.Contains(searchData)).ToList());
             }
         }
 
-        public ActionResult Info(int? id)
+        /// <summary>
+        /// Show customer detail
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
         {
-            return View();
+            FEViewModel myVM = new FEViewModel();
+
+            myVM.People = db.People.Find(id);
+
+            ViewBag.display = false;
+
+            //If bad input
+            if (id == null || myVM.People == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            else if(myVM.People.Customers2.Count() > 0)   //If Primary Contact People
+            {
+                //Ok to show
+                ViewBag.display = true;
+
+                //Find Customer
+                var customerID = myVM.People.Customers2.First().CustomerID;            
+                myVM.Customers = db.Customers.Find(customerID);
+
+                //LINQ Find Data
+                int orderCount = myVM.Customers
+                                     .Orders.SelectMany(i => i.Invoices)
+                                     .SelectMany(il => il.InvoiceLines)
+                                     .Count();
+                var grossSales = myVM.Customers
+                                     .Orders.SelectMany(i => i.Invoices)
+                                     .SelectMany(il => il.InvoiceLines)
+                                     .Sum(s => s.ExtendedPrice);
+                var grossProfit = myVM.Customers
+                                      .Orders.SelectMany(i => i.Invoices)
+                                      .SelectMany(il => il.InvoiceLines)
+                                      .Sum(s => s.LineProfit);
+                myVM.InvoiceLines = myVM.Customers
+                                        .Orders.SelectMany(i => i.Invoices)
+                                        .SelectMany(il => il.InvoiceLines)
+                                        .OrderByDescending(p => p.LineProfit)
+                                        .Take(10).ToList();
+
+                //Sign to ViewBag, ready return to UI
+                ViewBag.OrderCount = orderCount;
+                ViewBag.GrossSales = grossSales;
+                ViewBag.GrossProfit = grossProfit;
+
+            }
+
+            return View(myVM);
         }
     }
 }
