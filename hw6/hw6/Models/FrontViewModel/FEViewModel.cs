b8cdc3b5e368a6139d7bb30430d03d57bae8d5c1
YUMIN <ychen13@wou.edu>
2018-11-12 04:08:59 -0800

    Fixed some bug

--- hw6/hw6/Models/FrontViewModel/FEViewModel.cs
+++ hw6/hw6/Models/FrontViewModel/FEViewModel.cs
@@ -8,11 +8,13 @@ namespace hw6.Models.FrontViewModel
 {
     public class FEViewModel
     {
-
+        //Model from People 
         public People People { get; set; }
 
+        //Model from Customer
         public Customers Customers { get; set; }
 
+        //Model from InvoiceLines
         public List<InvoiceLines> InvoiceLines { get; set; }
     }
 }
